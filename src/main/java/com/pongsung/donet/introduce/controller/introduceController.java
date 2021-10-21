bf16e798a0b1e196422b8c56b74311aeae2ca952
sangyejin <oasis21213@naver.com>
2021-10-21 16:19:25 +0900

    yejin push

---
+++ src/main/java/com/pongsung/donet/introduce/controller/introduceController.java
@@ -0,0 +1,20 @@
+package com.pongsung.donet.introduce.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class introduceController {
+
+	@RequestMapping("introduce.pr")
+	public String introduceProject() {
+
+		return "introduce/introduceProject";
+	}
+
+	@RequestMapping("introduce.how")
+	public String introduceHowto() {
+
+		return "introduce/howtoDonation";
+	}
+}
