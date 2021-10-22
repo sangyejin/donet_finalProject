75007bb5d4ecbbce4a3bc3296bfc0f83a9af35e1
sangyejin <oasis21213@naver.com>
2021-10-22 23:10:13 +0900

    후원메인리스트 추가

--- src/main/java/com/pongsung/donet/donation/model/vo/SupportUsePlan.java
+++ src/main/java/com/pongsung/donet/donation/model/vo/SupportUsePlan.java
@@ -1,7 +1,5 @@
 package com.pongsung.donet.donation.model.vo;
 
-import java.sql.Date;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,10 +11,11 @@ import lombok.ToString;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-public class Sponsor {
-	private String userId; //후원인(사용자)
-	private int suNo; //후원 프로젝트 번호
-	private int payNo; //결제수단 번호
-	private int amount; //결제금액
-	private int total;
+public class SupportUsePlan {
+
+	private int suNo;
+	private int upNo;
+	private String content;
+	private int amount;
+	private String division;
 }
