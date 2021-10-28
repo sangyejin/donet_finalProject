71c5ab33ebf37a2e53f229529d9ac5f6c82826ce
taehyeonbang <zmfozjko0465@gmail.com>
2021-10-28 09:20:46 +0900

    커밋 엔 푸쉬를 진행함

--- src/main/java/com/pongsung/donet/attendance/controller/AttendanceController.java
+++ src/main/java/com/pongsung/donet/attendance/controller/AttendanceController.java
@@ -1,16 +1,21 @@
 package com.pongsung.donet.attendance.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.pongsung.donet.attendance.service.AttendanceService;
 import com.pongsung.donet.attendance.service.AttendanceServiceImpl;
+import com.pongsung.donet.member.controller.MemberController;
 
 @Controller
 @RequestMapping("attendance/Calendar")
 public class AttendanceController {
-
+	
+	private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
+	
 	@Autowired
 	private AttendanceService attendanceService;
 	
